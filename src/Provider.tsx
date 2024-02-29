276c46fe6e4ddc9ee778aaaeec6d515b6a1a97e9
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-02-29 07:21:52 +0100

    Structure de base.

---
+++ src/Provider.tsx
@@ -0,0 +1,9 @@
+import { router } from './router';
+import { ChakraProvider } from '@chakra-ui/react';
+import { RouterProvider } from 'react-router-dom';
+
+export const Provider = () => (
+  <ChakraProvider>
+    <RouterProvider router={router} />
+  </ChakraProvider>
+);
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Provider } from './Provider.tsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider />
+  </React.StrictMode>
+);

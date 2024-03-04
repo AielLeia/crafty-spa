d0b9d9f045831c1843a79d1938aeb1cb296a8c15
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-03-04 17:44:44 +0100

    Mise en place du layout de la page de profile.

---
+++ src/pages/Profile/ProfileTimeline/ProfileTimeline.tsx
@@ -0,0 +1,5 @@
+import { PostList } from '@/components/PostList.tsx';
+
+export const ProfileTimeline = () => {
+  return <PostList messages={[]} />;
+};
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,9 @@ import { AppStore } from '@/libs/create-store.ts';
 import { Home } from '@/pages/Home';
 import { createHomeLoader } from '@/pages/Home/create-home-loader.ts';
 import { Login } from '@/pages/Login.tsx';
+import { ProfileLayout } from '@/pages/Profile/ProfileLayout.tsx';
+import { ProfileTimeline } from '@/pages/Profile/ProfileTimeline/ProfileTimeline.tsx';
+import { createProfileTimelineLoader } from '@/pages/Profile/ProfileTimeline/create-profile-timeline-loader.ts';
 import { ProtectedPageLayout } from '@/pages/ProtectedPageLayout.tsx';
 import { RedirectHome } from '@/pages/RedirectHome.tsx';
 import { createBrowserRouter } from 'react-router-dom';
@@ -27,6 +30,25 @@ export const createRouter = (
           path: 'home',
           loader: createHomeLoader({ store }),
           element: <Home />,
+        },
+        {
+          path: 'u/:userId',
+          element: <ProfileLayout />,
+          children: [
+            {
+              index: true,
+              element: <ProfileTimeline />,
+              loader: createProfileTimelineLoader({ store }),
+            },
+            {
+              path: 'following',
+              element: <p>Following</p>,
+            },
+            {
+              path: 'followers',
+              element: <p>Followers</p>,
+            },
+          ],
         },
       ],
     },
